15ca3b2519fdb9fd43b928cfb12b733d8384ed65
Nguyễn Thành Hưng <92497428+thanhhungg@users.noreply.github.com>
2023-11-19 10:13:57 +0700

    heo

--- home/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class users(models.Model):
-    name = models.CharField(max_length=40)
-    account = models.CharField(max_length=40)
-    password = models.CharField(max_length=40)
-    lc_number = models.CharField(max_length=40)
-    age = models.IntegerField()
-    email = models.EmailField()
--- home/serializes.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import users
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = users
-        fields = ('name', 'lc_number', 'age', 'email')
